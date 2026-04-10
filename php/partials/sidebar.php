0b25c8f31368df6ce1580b4f3e042abc721a33e6
YuuY314 <rafaelyu0202@gmail.com>
2026-04-10 08:34:01 -0300

    Minor Update

---
+++ php/partials/sidebar.php
@@ -0,0 +1,9 @@
+<aside class="sidebar">
+    <a href="index.php" class="logo">ConstruTech</a>
+    <nav>
+        <ul class="links">
+            <li><a href="index.php">Produtos</a></li>
+            <li><a href="register_product.php">Cadastrar Produtos</a></li>
+        </ul>
+    </nav>
+</aside>
