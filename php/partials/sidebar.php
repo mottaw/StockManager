aced6146c777cf12f0a0520cad0774557039ce76
YuuY314 <rafaelyu0202@gmail.com>
2026-04-15 10:37:29 -0300

    Updated Sidebar

--- php/partials/sidebar.php
+++ php/partials/sidebar.php
@@ -3,16 +3,19 @@ $currentPage = basename($_SERVER["PHP_SELF"]);
 ?>
 
 <aside class="sidebar">
-    <a href="index.php" class="logo">ConstruTech</a>
+    <a href="index.php" class="logo">
+        <img src="img/logo.png">
+        <p>Constru<span>Tech</span></p>
+    </a>
     <nav>
         <ul class="links">
-            <li class="focus">
+            <li class="<?= $currentPage == "index.php" ? 'focus' : '' ?>">
                 <a href="index.php">
                     <i class="fa-solid fa-house"></i>
                     <span>Início</span>
                 </a>
             </li>
-            <li>
+            <li class="<?= $currentPage == "register_product.php" ? 'focus' : '' ?>">
                 <a href="register_product.php">
                     <i class="fa-regular fa-pen-to-square"></i>
                     <span>Cadastrar</span>
